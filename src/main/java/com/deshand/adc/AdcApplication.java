759cb775329cb11b06c6487af560f6d4ed677e56
Ajaks84 <x2p@tut.by>
2017-09-12 22:41:20 +0300

    first commit

---
+++ src/main/java/com/deshand/adc/AdcApplication.java
@@ -0,0 +1,12 @@
+package com.deshand.adc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deshand/adc/AdcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deshand.adc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
